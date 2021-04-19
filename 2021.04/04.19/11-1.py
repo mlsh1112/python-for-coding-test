c5e9352672886031de8b1117b8cab9b2b00954d1
mlsh1112 <mlsh1112@ajou.ac.kr>
2021-04-19 15:08:46 +0000

    04.19

---
+++ 2021.04/04.19/11-1.py
@@ -0,0 +1,12 @@
+n=int(input())
+num=list(map(int,input().split()))
+stack=[]
+
+result=[-1 for _ in range(n)]
+
+for i in range(len(num)):
+  while len(stack) and num(stack[-1])<num[i]:
+    result[stack.pop()]=num[i]
+  stack.append(i)
+
+print(*result)
